3b9a3a07318161a925d233fc8a41640ac5b4dca1
sagnik <santrasagnik10@gmail.com>
2023-03-18 01:06:35 +0530

    changed contents

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,12 +12,10 @@ export default function Home() {
         <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
           <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
             <h1 className="title-font font-medium text-3xl text-gray-900">
-              Slow-carb next level shoindcgoitch ethical authentic, poko
-              scenester
+              Log into a World Full of Opportunities
             </h1>
             <p className="leading-relaxed mt-4">
-              Poke slow-carb mixtape knausgaard, typewriter street art gentrify
-              hammock starladder roathse. Craies vegan tousled etsy austin.
+            We believe in unlocking the full potential of women as entrepreneurs and leaders, driving innovation and economic growth. We strive to create a more equitable world by providing women with access to technology and the skills needed to succeed in the digital age. Through our efforts, we aim to build a better future for all.
             </p>
           </div>
           <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
