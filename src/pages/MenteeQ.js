6a48f59badee9b64959dcf6be8843d8f3cd0b6c9
sagnik <santrasagnik10@gmail.com>
2023-03-18 15:57:45 +0530

    added Firebase to MenteeQ

--- src/pages/MenteeQ.js
+++ src/pages/MenteeQ.js
@@ -5,6 +5,27 @@ import { db } from "../../firebase";
 import { addDoc, collection } from "firebase/firestore";
 
 const MenteeQ = () => {
+  const i = async () => {
+    const nameRef = await addDoc(collection(db, "forms"), {
+      firstName: firstNameValue,
+      lastName: lastNameValue,
+    });
+
+    return nameRef;
+  };
+  const [firstNameValue, setfirstNameValue] = useState();
+  const [lastNameValue, setlastNameValue] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    i()
+      .then(() => {
+        console.log("Radiohead value saved!");
+      })
+      .catch((error) => {
+        console.error("Error writing document:", error);
+      });
+  };
   return (
     <div>
       <section class="text-gray-600 body-font">
@@ -14,7 +35,7 @@ const MenteeQ = () => {
               
             </h2>
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Fill this Form to get matched with a Mentor
+            Fill this form to get a Mentor
             </h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
               
@@ -22,6 +43,46 @@ const MenteeQ = () => {
           </div>
           <div class="flex flex-wrap flex-col justify-center content-center">
             <div class="xl:w-2/4 lg:w-1/2 md:w-full px-8 py-6 flex flex-col items-center">
+            <form className="w-full max-w-lg" onSubmit={handleSubmit}>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-first-name"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                      id="grid-first-name"
+                      type="text"
+                      placeholder="Jane"
+                      value={firstNameValue}
+                      onChange={(e) => setfirstNameValue(e.target.value)}
+                    />
+                  </div>
+                  <div class="w-full md:w-1/2 px-3">
+                    <label
+                      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                      for="grid-last-name"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                      id="grid-last-name"
+                      type="text"
+                      placeholder="Doe"
+                      value={lastNameValue}
+                      onChange={(e) => setlastNameValue(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <button class="ml-0 inline-flex text-white bg-pink-500 border-0 py-0.3 px-3 focus:outline-none hover:bg-pink-300 rounded text-lg" type="submit">Save</button>
+                <h1 class="text-white">...</h1>
+                <h1 class="text-white">...</h1>
+              </form>
+
               <Form 
               question={"Skills you want to learn"}
               o1={"Technical"}
