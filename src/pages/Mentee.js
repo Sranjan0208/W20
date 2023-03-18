a72b183233ed1d95969f662f0dcff211c1663bab
Milind <milindjha1@gmail.com>
2023-03-18 15:59:55 +0530

    mentee and mentor updated

--- src/pages/Mentee.js
+++ src/pages/Mentee.js
@@ -9,50 +9,50 @@ const Mentee = () => {
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap w-full mb-20">
       <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Connecting you with Opportunities</h1>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">My Instructors</h1>
         <div class="h-1 w-20 bg-pink-500 rounded"></div>
       </div>
-      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
-    </div>
-    <div class="flex flex-wrap -m-4">
+          </div>
+    <div class="flex flex-wrap m-4">
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"/>
-          <h3 class="tracking-widest text-pink-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg" alt="content"/>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Rekha</h2>
         </div>
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
-          <h3 class="tracking-widest text-pink-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg" alt="content"/>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Jaya</h2>
         </div>
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
-          <h3 class="tracking-widest text-pink-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg" alt="content"/>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Hema</h2>
         </div>
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
-          <h3 class="tracking-widest text-pink-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg" alt="content"/>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Pushpa</h2>
         </div>
       </div>
     </div>
   </div>
 </section>
+
+
+
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
+  <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Schemes to refer to</h1>
+        <div class="h-1 w-20 bg-pink-500 rounded"></div>
+      </div>
+          </div>
+    <div class="flex flex-wrap m-4">
       <div class="p-4 lg:w-1/3">
         <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
           <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Government Scheme</h2>
@@ -83,7 +83,7 @@ const Mentee = () => {
       </div>
       <div class="p-4 lg:w-1/3">
         <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Government Scheme</h2>
           <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Annapurna Scheme</h1>
           <p class="leading-relaxed mb-3">The Annapurna Yojana is one of the many government schemes for women. Under this, the Government of India offers women entrepreneurs schemes in food catering businesses loans up to Rs. 50,000. The borrowed amount could be used for working capital requirements such as buying utensils, mixer cum grinder, hot case, tiffin boxes, working table, etc. </p>
           <a class="text-pink-500 inline-flex items-center">Learn More
@@ -109,9 +109,9 @@ const Mentee = () => {
       </div>
       <div class="p-4 lg:w-1/3">
         <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Selvage Poke Waistcoat Godard</h1>
-          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Government Scheme</h2>
+          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Stree Shakti Yojana</h1>
+          <p class="leading-relaxed mb-3">This woman loan scheme is eligible for women who have the majority of ownership in the business. Another requirement is that these women entrepreneurs be enrolled under the Entrepreneurship Development Programme (EDP) organised by their respective state agencies. This Stree Shakti scheme allows women to avail of an interest concession of 0.05% on loans more than Rs. 2 lakh.</p>
           <a class="text-pink-500 inline-flex items-center">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
